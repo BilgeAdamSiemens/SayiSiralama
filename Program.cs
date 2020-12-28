1072423939a5e707e8d4450438f11c7ce14c4f98
gulenzek <73951604+gulenzek@users.noreply.github.com>
2020-12-28 22:48:02 +0300

    Ödevin son hali..

--- Program.cs
+++ Program.cs
@@ -9,16 +9,35 @@ namespace Odev_10
             Console.WriteLine("5 Sayılı Dizi Sıralama ve Aralarındaki Sayıları Bulma");
             int min;
             int max;
-            int[] dizi = { 13, 19, -5, 89, 70 };
-            DiziYazdir(dizi); // Önce diziyi yazdırıyoruz.
-            Sirala(dizi, out min, out max); // Diziyi sıralıyoruz
-            DiziYazdir(dizi);
-            DiziYazdir(dizi, min, max);
-            Console.WriteLine($"{dizi} --> {min} - {max}");
+            // int[] dizi = { 13, 19, -5, 89, 70 }; // Aşağıda kullanıcıdan alıyoruz.
+            try
+            {
+                Console.WriteLine("Sayıları giriniz:");
+                string str = Console.ReadLine();
+                char[] ayracKarakterler = { ' ', ',', '.', ':', '\t' };
+
+                string[] stringDizi = str.Split(ayracKarakterler);
+                int[] dizi = new int[5];
+
+                for (int k = 0; k < 5; k++)
+                {
+                    dizi[k] = int.Parse(stringDizi[k]);
+                }
+                DiziYazdir(dizi); // Önce diziyi yazdırıyoruz.
+                Sirala(dizi, out min, out max); // Diziyi sıralıyoruz
+                DiziYazdir(dizi);
+                DiziYazdir(dizi, min, max);
+                Console.WriteLine($"{dizi} --> {min} - {max}");
+            }
+            catch(Exception e)
+            {
+                throw new ArgumentException();
+            }
         }
 
         static void Sirala(int[] dizi, out int sayiMinimum, out int sayiMaksimum)
         {
+            // Bubble Sort algoritması kullanarak, dizi, küçükten büyüğe doğru sıralandı.
             int sayac = dizi.Length;
             for (int i = 0; i < sayac - 1; i++)
             {
@@ -26,9 +45,9 @@ namespace Odev_10
                 {
                     if (dizi[j] > dizi[j + 1])
                     {
-                        int temp = dizi[j];
+                        int geciciSayi = dizi[j];
                         dizi[j] = dizi[j + 1];
-                        dizi[j + 1] = temp;
+                        dizi[j + 1] = geciciSayi;
                     }     
                 }
             }
@@ -46,9 +65,9 @@ namespace Odev_10
             for (int i = 0; i <= (max - min); i++)
             {
                 if(min+i==dizi[0]|| min + i == dizi[1] || min + i == dizi[2] || min + i == dizi[3] || min + i == dizi[4])
-                    Console.WriteLine($"===> {min + i}");
+                    Console.WriteLine($"===> {min + i}"); // Dizide yer alan veriler '===>' işareti ile yazdırılır.
                 else
-                    Console.WriteLine($"---> {min + i}");
+                    Console.WriteLine($"---> {min + i}"); // Arada yer alan sayılar '--->' işareti ile yazdırılır.
             }
         }
 
